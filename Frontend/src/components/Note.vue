1cb4045f7ff8a7df9c35a69eae4cf21e0844af0d
marusegoviac <mrsegovia6@gmail.com>
2024-02-04 13:38:04 -0400

    added new component (Note)

---
+++ Frontend/src/components/Note.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="bg-mint h-20 w-full mb-5 py-3 px-5 rounded-md">
+    <div
+      class="mb-2"
+    >
+      <p class="text-gray text-md">{{ text }}</p>
+    </div>
+    <div>
+      <p class="text-medium-gray text-sm">{{ content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props:{
+    text:{
+      required: true,
+      type: String,
+    },
+    content: {
+      required: true,
+      type: String,
+    },
+  },
+  setup(props) {
+
+    return {
+      // Data
+    }
+  }
+}
+</script>
